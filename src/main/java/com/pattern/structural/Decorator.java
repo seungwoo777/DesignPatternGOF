6ef5d838a40bcbaef0bd6b78e15eab2121392f81
seungwoo <sw.nam@baumpns.com>
2024-12-10 00:40:38 +0900

    구조 -> 데코레이터 패턴 추가 구조 -> 플라이웨이트 패턴 추가

--- src/main/java/com/pattern/structural/Decorator.java
+++ src/main/java/com/pattern/structural/Decorator.java
@@ -1,7 +1,106 @@
 package com.pattern.structural;
 
-/*
- * 동적 기능 추가
+/* 데코레이터 패턴 (동적 기능 추가)
+ * 데코레이터 패턴은 대상 객체에 대한 기능 확장이나 변경이 필요할때 객체의 결합을 통해 서브클래싱 대신 쓸 수 있는 유연한 대안 구조
+ * 데코레이터 패턴을 이용 시 필요한 추가 기능의 조합을 런타임에서 동적으로 생성 할 수 있음. 데코레이터할 대상 객체를 새로운 행동들을 포함한 특수
+ * 장식자 객체에 넣어서 행동들을 해당 장식자 객체마다 연결 시켜, 서브클래스로 구성할 때 보다 훨씬 유연하게 기능을 확장 할 수 있음.
+ * 기능을 구현하는 클래스들을 분리함으로써 수정이 용이
  */
 public class Decorator {
+    // Client
+    public static void main(String[] args) {
+
+        // 1. 원본객체 생성
+        IComponent component = new ConcreteComponent();
+
+        // 2. 장식 1 하기
+        IComponent decorator1 = new ComponentDecorator1(component);
+        decorator1.operation(); // 장식된 객체의 장식된 기능 실행
+
+        // 3. 장식 2 하기
+        IComponent decorator2 = new ComponentDecorator2(component);
+        decorator2.operation(); // 장식된 객체의 장식된 기능 실행
+
+        // 4. 장식 1 + 2 하기
+        IComponent decorator3 = new ComponentDecorator1(new ComponentDecorator2(component));
+    }
 }
+
+/* 데코레이터 패턴 구조
+ *  1. Component : 원본 객체와 장식된 객체 모두를 묶는 역할
+ *  2. ConcreteComponent : 원본 객체(데코레이팅 할 객체)
+ *  3. Decorator : 추상화된 장식자 클래스
+ *      3-1 원본 객체를 합성한 wrapper 필드와 인터페이스의 구현 메소드를 가지고 있음.
+ *  4. ConcreteDecorator : 구체적인 장식자 클래스
+ *      4-1 부모 클래스가 감싸고 있는 하나의 Component 를 호출하면서 호출 전/후로 부가적인 로직을 추가
+ */
+interface IComponent {
+    void operation();
+}
+
+class ConcreteComponent implements IComponent {
+    public void operation() {}
+}
+
+abstract class AbstractDecorator implements IComponent {
+    // 원본 객체를 합성
+    IComponent wrapper;
+
+    AbstractDecorator(IComponent wrapper) {
+        this.wrapper = wrapper;
+    }
+
+    public void operation() {
+        // 위임
+        wrapper.operation();
+    }
+}
+
+class ComponentDecorator1 extends AbstractDecorator {
+
+    ComponentDecorator1(IComponent wrapper) {
+        super(wrapper);
+    }
+
+    public void operation() {
+        super.operation(); // 원본 객체를 상위 클래스의 위임을 통해 실행
+        extraOperation(); // 장식 클래스만의 메소드를 실행
+    }
+
+    void extraOperation() {}
+}
+
+class ComponentDecorator2 extends AbstractDecorator {
+
+    ComponentDecorator2(IComponent wrapper) {
+        super(wrapper);
+    }
+
+    public void operation() {
+        super.operation(); // 원본 객체를 상위 클래스의 위임을 통해 실행
+        extraOperation(); // 장식 클래스만의 메소드를 실행
+    }
+
+    void extraOperation() {}
+}
+
+/* Decorator 패턴 특징
+ * 패턴 사용시기
+ *  1. 객체 책임과 행동이 동적으로 상황에 따라 다양한 기능이 빈번하게 추가/삭제되는 경우
+ *  2. 객체의 결합을 통해 기능이 생성될 수 있는 경우
+ *  3. 객체를 사용하는 코드를 손상시키지 않고 런타임에 객체에 추가 동작을 할당할 수 있어야 하는 경우
+ *  4. 상속을 통해 서브클래싱으로 객체의 동작을 확장하는 것이 어색하거나 불가능 할 때
+ *
+ * 장점
+ *  1. 데코레이터를 사용하면 서브클래스를 만들때보다 훨씬 더 유연하게 기능을 확장할 수 있음
+ *  2. 객체를 여러 데코레이터로 래핑하여 여러 동작을 결합할 수 있음
+ *  3. 컴파일 타임이 아닌 런타임에 동적으로 기능을 변경할 수 있음
+ *  4. 각 장식자 클래스마다 고유의 책임을 가져 단일 책임 원칙(SRP)을 준수
+ *  5. 클라이언트 코드 수정없이 기능 확장이 필요하면 장식자 클래스를 추가하면 되니 개방 폐쇄 원칙(OCP)을 준수
+ *  6. 구현체가 아닌 인터페이스를 바라봄으로써 의존 역전 원칙(DIP) 준수
+ *
+ * 단점
+ *  1. 만일 장식자 일부를 제거하고 싶다면, Wrapper 스택에서 특정 wrapper 를 제거하는 것은 어려움
+ *  2. 데코레이터를 조합하는 초기 생성코드가 보기 안좋을 수 있다. new A(new B(new C(new D())))
+ *  3. 어느 장식자를 먼저 데코레이팅 하느냐에 따라 데코레이터 스택 순서가 결정지게 되는데, 만일 순서에 의존하지 않는 방식으로 데코레이터를 구현하기는 어려움.
+ */
--- src/main/java/com/pattern/structural/Flyweight.java
+++ src/main/java/com/pattern/structural/Flyweight.java
@@ -1,7 +1,102 @@
 package com.pattern.structural;
 
-/*
- * 공유 객체를 활용해 메모리 절약
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 플라이웨이트 패턴(공유 객체를 활용해 메모리 절약)
+ * 재사용 가능한 객체 인스턴스를 공유시키 메모리 사용량을 최소화하는 구조 패턴
+ * 간단히 요약하면 캐시(Cache) 개념을 코드로 패턴화 한것으로 보면 되는데
+ * 자주 변화는 속성과 변하 지않는 속성을 분리하고 변하지 않는 속성을 캐시하여 재사용해 메모리 사용을 줄이는 방식
+ * 그래서 동일하거나 유사한 객체들 사이에 가능한 많은 데이터를 서로 공유하여 사용하도록 하여 최적화를 노리는 경량 패턴이라고도 불림.
  */
 public class Flyweight {
+
+    // Client
+    public static void main(String[] args) {
+        FlyweightFactory flyweightFactory = new FlyweightFactory();
+
+        // 공유 Flyweight 객체
+        iFlyweight flyweight1 = flyweightFactory.getFlyweight("A");
+        iFlyweight flyweight2 = flyweightFactory.getFlyweight("B");
+        iFlyweight flyweight3 = flyweightFactory.getFlyweight("A"); // 이미 생성된 객체 재사용
+
+        flyweight1.render("Position 1");
+        flyweight2.render("Position 2");
+        flyweight3.render("Position 3");
+
+
+        // 비공유 Flyweight 객체
+        iFlyweight unsharedFlyweight = new UnsharedConcreteFlyweight("unique");
+        unsharedFlyweight.render("Position 4");
+
+    }
 }
+
+/* 플라이웨이트 패턴 구조
+ *  1. Flyweight : 경량 객체를 묶는 인터페이스
+ *  2. ConcreteFlyWeight : 공유 가능하여 재사용 되는 객체 (intrinsic state)
+ *  3. UnsharedConcreteFlyweight : 공유 불가능한 객체 (extrinsic state)
+ *  4. FlyweightFactory : 경량 객체를 만드는 공장역할과 캐시 역할을 겸비하는 Flyweight 객체 관리 클래스
+ *      4-1 getFlyweight() 메소드는 팩토리 메소드 역할을 한다고 보면 됨
+ *      4-2 만일 객체가 메모리에 존재하면 그대로 가져와 반환하고 없다면 새로 생성해 반환
+ *  5. Client : 클라이언트는 FlyweightFactory 를 통해 Flyweight 타입의 객체를 얻어 사용한다.
+ */
+
+// 공통 인터페이스
+interface iFlyweight {
+    void render(String extrinsicState); // 외부 상태 (위치, 색상 등) 전달
+}
+
+//  공유 가능한 객체
+@AllArgsConstructor
+class ConcreteFlyweight implements iFlyweight {
+    private final String intrinsicState;
+
+
+    public void render(String extrinsicState) {
+        System.out.println("렌더링 플라이웨이트 intrinsicState -> " + intrinsicState + " extrinsicState ->" + extrinsicState);
+    }
+}
+
+// 공유 되지 않는 객체
+@AllArgsConstructor
+class UnsharedConcreteFlyweight implements iFlyweight {
+    private final String uniqueState;
+
+    public void render(String extrinsicState) {
+        System.out.println("렌더링 비공유 플라이 웨이트" + uniqueState + " extrinsicState ->" + extrinsicState);
+    }
+}
+
+// 경량 객체를 관리하는 팩토리
+class FlyweightFactory {
+    private final Map<String, iFlyweight> flyweight = new HashMap<>();
+
+    public iFlyweight getFlyweight(String key) {
+        if(!flyweight.containsKey(key)) {
+            System.out.println("새로운 플라이웨이트 키 생성 -> " + key);
+            flyweight.put(key , new ConcreteFlyweight(key));
+        } else {
+            System.out.println("재사용 존재하는 플라이웨이트 키 -> " + key);
+        }
+        return flyweight.get(key);
+    }
+}
+
+/*
+ * 특징
+ *  사용시기
+ *  1. 어플리케이션에 의해 생성되는 객체의 수가 많아 저장 비용이 높아 질 때
+ *  2. 생성된 객체가 오래도록 메모리에 상주하며 사용되는 횟수가 많을 때
+ *  3. 공통적인 인스턴스를 많이 생성하는 로직이 포함된 경우
+ *  4. 임베디드와 같이 메모리를 최소한 사용해야 하는 경우에 활용
+ * 장점
+ *  1. 애플리케이션에서 사용하는 메모리를 줄일 수 있음
+ *  2. 프로그램 속도를 개선 할 수 있다.
+ *      2-1 new 로 인스턴스화를 하면 데이터가 생성되고 메모리에 적재 되는 미령의 시간이 걸림
+ *      2-2 객체를 공유하면 인스턴스를 가져오기만 하면 되기 때문에 메모리 뿐만 아니라 속도도 향상 시킬 수 있게 됨
+ * 단점
+ *  1. 코드 복잡도가 증가
+ */
